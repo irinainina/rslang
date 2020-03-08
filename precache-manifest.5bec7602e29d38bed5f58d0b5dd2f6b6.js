fc092da67b538b094d9d1747d156fd006f0cae2a
irinainina <irbris@mail.ru>
2020-03-08 20:42:46 +0200

    feat: add word list

---
+++ precache-manifest.5bec7602e29d38bed5f58d0b5dd2f6b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3890265c83ef2e563ede6e210a210044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1284eb7898dfabdf7e78",
+    "url": "/static/css/main.d3fe5977.chunk.css"
+  },
+  {
+    "revision": "c50c3a08e89724cd0283",
+    "url": "/static/js/2.29c3cea7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.29c3cea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1284eb7898dfabdf7e78",
+    "url": "/static/js/main.95858909.chunk.js"
+  },
+  {
+    "revision": "b49f1054978fd54a024e",
+    "url": "/static/js/runtime-main.f4ea150f.js"
+  }
+]);
